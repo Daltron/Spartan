478d3398c803deaa6d72455c48f593d2c3083b97
dhint4 <daltonhint4@gmail.com>
2017-01-29 20:11:06 -0600

    removed view controller code

--- Example/Spartan/ViewController.swift
+++ Example/Spartan/ViewController.swift
@@ -11,11 +11,5 @@ import Spartan
 
 class ViewController: UIViewController {
 
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        Spartan.authorizationToken =    "BQBIk_k00E1SYvbGRxbpp4_VzNVR_88zSdkTyNE4LFtLj1h2qH0Cvib5s4ymC8-QvCG1oKyuDLXFbit3wdXniVqMhrXxELHRccxOM89kiLo2DeUp3Y7KAcuq2jkVE5sgScxWXZFpEIUOjvQ3KpXQVOLG8k3K0LQxcKeZJret--HawN0-6CJNfWXSVLCbdnQ5wY8-vrGVTHXRvbQ5FFSfmJpveyial_eoTecyxntihwZtUu6aGdhIXcL6mzkZiXrwF1rPIG3QS-mTBdjTFaOGo5-sd58cQOFKavveXXQ_T1zQxBWn"
-        
-    }
 }
 
